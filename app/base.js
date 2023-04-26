15f608069c3e3306f13e0dba0ad172d92ce2e285
MarcellKov <kmarcell379@gmail.com>
2023-04-26 20:56:02 +0200

    first commit

---
+++ app/base.js
@@ -0,0 +1,16 @@
+import fast from "fastify"
+import {createConnection} from "mysql"
+
+const conn=createConnection({host:"mysql",port:"3306",database:"PROBA",user:"proba",password:"Matyas53",})
+
+const app=fast()
+
+app.listen({port:parseInt(process.argv[2]),host:"0.0.0.0"})
+console.log(process.argv[2])
+app.get("/",()=>{return "Server from"+process.argv[2]})
+app.get("/db",()=>{
+    conn.query("SELECT * from emberek",function e(err,results){
+        if (err) throw err
+        console.log(results)
+    })
+})
