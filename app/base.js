6ffac2c2a4d6288d7645bba5115c2962acf21a58
MarcellKov <kmarcell379@gmail.com>
2023-05-01 08:13:34 +0200

    clean

--- app/base.js
+++ app/base.js
@@ -1,16 +1,14 @@
-import fast from "fastify"
-import {createConnection} from "mysql"
-
-const conn=createConnection({host:"mysql",port:"3306",database:"PROBA",user:"root",password:"Matyas53",})
+const fast=require("fastify")
+//const conn=createConnection({host:"mysql",port:"3306",database:"PROBA",user:"root",password:"Matyas53",})
 
 const app=fast()
-
+app.register(require("@fastify/cors"),{origin:'*',methods:["GET"]})
 app.listen({port:parseInt(process.argv[2]),host:"0.0.0.0"})
 console.log(process.argv[2])
-app.get("/",()=>{return "Server from"+process.argv[2]})
-app.get("/db",()=>{
-    conn.query("SELECT * from emberek",function e(err,results){
-        if (err) throw err
-        console.log(results)
-    })
-})
+app.get("/",()=>{return "AZURE Server from"+process.argv[2]})
+//app.get("/db",()=>{
+//    conn.query("SELECT * from emberek",function e(err,results){
+//        if (err) throw err
+//        console.log(results)
+//    })
+//})
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,59 +1,13 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script >
+    import axios from "axios";
+
+    //import axios from "axios"
+    import { onMount } from "svelte";
+    onMount(function e(){axios.get("http://localhost:80/",{url:"http://localhost:80/"}).then(res=>{console.log(res)}).catch(err=>{console.log(err)})})
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<h1>WELCOMEa</h1>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<a href="about">
+    <h1>ROUTER</h1>
+</a>
